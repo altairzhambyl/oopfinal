3d6673c56f320eacc342261408618492eb892f01
Altair Zhambyl <altairzhambyl@gmail.com>
2024-12-21 13:09:23 +0600

    chore: stable checkpoint

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -1,9 +1,6 @@
 package Driver;
-
-<<<<<<< HEAD
 import OfficeRegistrar.*;
 import Users.*;
-=======
 import OfficeRegistrar.Course;
 import Pages.Navigation;
 import Users.*;
@@ -12,7 +9,6 @@ import Database.DataBase;
 import Enums.*;
 
 import java.io.IOException;
->>>>>>> 3ee5714b0c8d7a2d4a0cea4b1c9107f19d7c1c1e
 import java.util.*;
 
 
--- src/Users/Student.java
+++ src/Users/Student.java
@@ -1,15 +1,7 @@
 package Users ;
-
-
-<<<<<<< HEAD
-public class Student extends User
-{	
-=======
 import OfficeRegistrar.*;
 
-public abstract class Student extends User
-{	
-	
->>>>>>> 3ee5714b0c8d7a2d4a0cea4b1c9107f19d7c1c1e
+public class Student extends User {	
+
 	
 }
